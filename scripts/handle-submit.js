d2032f439f49a8dd18ba321e1e848aa9336eae46
Jay Chu <tothesong@gmail.com>
2019-05-03 00:28:52 +0800

    Extract label

--- scripts/handle-submit.js
+++ scripts/handle-submit.js
@@ -28,35 +28,35 @@ function handleSubmit(event) {
     <h2>个人基本信息</h2>
 
     <div>
-      <label for='name'>姓名：</label>
+      姓名：
       <input type='text' name='cv_name' id='name' placeholder='请在此输入姓名' required>
     </div>
 
     <div>
       性别：
       <input type='radio' name='cv_gender' id='gender_male' value='男' checked>
-      <label for='gender_male'>男</label>
+      男
       <input type='radio' name='cv_gender' id='gender_female' value='女'>
-      <label for='gender_female'>女</label>
+      女
     </div>
 
     <div>
-      <label for='avatar'>上传头像：</label>
+      上传头像：
       <input type='file' name='cv_avatar' id='avatar' accept='images/*'>
     </div>
 
     <div>
-      <label for='email'>电子邮箱：</label>
+      电子邮箱：
       <input type='email' name='cv_mail' id='mail' placeholder='somebody@example.com' required>
     </div>
 
     <div>
-      <label for='telephone'>联系电话：</label>
+      联系电话：
       <input type='tel' name='cv_telephone' id='telephone' required>
     </div>
 
     <div>
-      <label for='birthday'>出生日期：</label>
+      出生日期：
       <input type='date' name='cv_birthday' id='birthday' required>
     </div>
   </section>
@@ -65,23 +65,23 @@ function handleSubmit(event) {
     <h2>学习以及实习经历</h2>
 
     <div>
-      <label for='degree'>学历程度</label>
+      学历程度
       <select name='cv_degree' id='degree'>
       </select>
     </div>
 
     <div id='experienceList'>
-      <label for='experience'>学习与工作履历：</label>
+      学习与工作履历：
     </div>
 
     <div>
-      <label for='expectation'>自我陈述：（不少于20字）</label>
+      自我陈述：（不少于20字）
       <br>
       <textarea name='cv_expectation' id='expectation' placeholder='请填写对于所申请岗位的理解，以及对未来的展望。' minlength='20' required></textarea>
     </div>
 
     <div>
-      <label for='hobby'>兴趣特长：</label>
+      兴趣特长：
       <br>
       <textarea name='cv_hobby' id='hobby' placeholder='请填写您的特长，以及获奖项目等。'></textarea>
     </div>
